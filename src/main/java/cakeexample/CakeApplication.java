83d24ea36607f9fa9fda5861d0f4ec7c0b0e6fd6
thoraage <thoraageeldby@gmail.com>
2013-01-06 12:53:48 +0100

    Simplified cake-use

--- src/main/java/cakeexample/CakeApplication.java
+++ src/main/java/cakeexample/CakeApplication.java
@@ -2,13 +2,7 @@ package cakeexample;
 
 public class CakeApplication {
     static public void main(String[] args) {
-        CakeStack stack = new CakeStack();
-        for (String name : stack.getDb().getCakes()) {
-            System.out.println(name);
-        }
-        stack.getWebHandler().start();
-        stack.getWebHandler().join();
-        stack.getWebHandler().stop();
+        new CakeStack().getWebHandler().start();
     }
 }
 
